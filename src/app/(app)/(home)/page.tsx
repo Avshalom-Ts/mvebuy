8aa4036a5c9f0c63ef5255c8ee750b742f97ae97
Avshalom-Z <avshalomz.ts@gmail.com>
2025-04-23 22:19:46 +0300

    Refactor payload configuration and update category handling in routes

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,32 +1,19 @@
-import { Button } from '@/components/ui/button'
-import { Checkbox } from '@/components/ui/checkbox'
-import { Input } from '@/components/ui/input'
-import { Progress } from '@/components/ui/progress'
-import { Textarea } from '@/components/ui/textarea'
-import React from 'react'
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
 
-const Home = () => {
+export default async function Home () {
+  const payload = await getPayload({
+      config: configPromise,
+    })
+
+  const data = await payload.find({
+    collection: 'categories',
+  })
+
+
   return (
-    <div className='flex flex-col gap-y-4 p-4'>
-      <div>
-        <Button variant='elevated'>
-          I am a button
-        </Button>
-      </div>
-      <div>
-        <Input placeholder='I am an input'/>
-      </div>
-      <div>
-        <Progress value={50}/>
-      </div>
-      <div>
-        <Textarea placeholder='I am a textarea'/>
-      </div>
-      <div>
-        <Checkbox/>
-      </div>
+    <div>
+      {JSON.stringify(data,null, 2)}
     </div>
   )
 }
-
-export default Home
