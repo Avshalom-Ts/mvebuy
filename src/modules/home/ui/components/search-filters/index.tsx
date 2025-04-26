5508f8fa983ba19fce8a7db8415d07bd5fc97565
Avshalom-Z <avshalomz.ts@gmail.com>
2025-04-26 13:22:10 +0300

    feat(search-filters): enhance category filtering logic to dispaly category color on navbar and add default background color constant

--- src/modules/home/ui/components/search-filters/index.tsx
+++ src/modules/home/ui/components/search-filters/index.tsx
@@ -4,14 +4,31 @@ import { useTRPC } from "@/app/trpc/client";
 import { Categories } from "./categories";
 import { SearchInput } from "./search-input";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import { DEFAULT_BG_COLOR } from "@/modules/home/constanst";
 
 
 export const SearchFilters = () => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
+
+    const params = useParams()
+    const categoryParam = params.category as string | undefined
+    const activeCategory = categoryParam || 'all'
+
+    const activeCategoryData = data.find((category) => category.slug === activeCategory)
+
+    const activeCategoryColor = activeCategoryData?.color || DEFAULT_BG_COLOR
+    const activeCategoryName = activeCategoryData?.name || null
+
+    const activeSubcategory = params.subcategory as string | undefined
+    const activeSubcategoryName = activeCategoryData?.subcategories.find(
+        (subcategory) => subcategory.slug === activeSubcategory
+    )?.name || null 
+
     return(
         <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4" style={{
-            backgroundColor: '#F5F5F5',
+            backgroundColor: activeCategoryColor,
         }}>
             <SearchInput disabled/>
             <div className="hidden lg:block">
